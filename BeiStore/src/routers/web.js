1bf666a0c1448caa27a97c5e3f293169fdca192a
ErikSuca <erik.sucasai@gmail.com>
2021-06-17 18:01:57 -0300

    agregue rutas faltantes

--- BeiStore/src/routers/web.js
+++ BeiStore/src/routers/web.js
@@ -5,5 +5,9 @@ const path = require('path');
 
 // Routers 
 router.get('/home', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'home.html')));
+router.get('/productDetail', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'productDetail.html')));
+router.get('/productCart', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'productCart.html')));
+router.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'register.html')));
+router.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, '../views', 'login.html')));
 
 module.exports = router;
