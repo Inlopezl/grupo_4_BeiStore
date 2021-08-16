3e40475fa5d36f834d96eed281a74d03cea837e6
Inlopezl <ivanxlopez@gmail.com>
2021-08-16 13:53:01 -0300

    No pasa el error de validacion a la vista

--- BeiStore/src/controllers/userController.js
+++ BeiStore/src/controllers/userController.js
@@ -1,5 +1,5 @@
 const user = require('../models/users');
-
+const { validationResult } = require('express-validator');
 
 module.exports = {
     indexLogin: (req, res) => {
@@ -15,13 +15,17 @@ module.exports = {
     },
 
     indexEdit: (req, res) => {
-        res.render('users/form', { action: `/users/update/${product.one(req.params.id).id}?_method=PUT`, typePage: 'edit', title: 'Editar usuario', user: user.one(req.params.id) });
+        res.render('users/register', { action: `/users/update/${product.one(req.params.id).id}?_method=PUT`, typePage: 'edit', title: 'Editar usuario', user: user.one(req.params.id) });
     },
 
     save: (req, res) => {
-        console.log(req.body);
-        let newUser = user.new(req.body, req.file);
-        return newUser == true ? res.redirect('/home') : res.send('Error');
+        let error = validationResult(req);
+        if (error.errors.length > 0) {
+            return res.render("users/register"), { errors: error.mapped() }
+        } else {
+            let newUser = usernew(req.body, req.file);
+            return newUser == true ? res.redirect('/home') : res.send('Error');
+        }
     },
     update: (req, res) => {
         let editUser = user.edit(req.body, req.file, req.params.id);
--- BeiStore/src/routers/user.js
+++ BeiStore/src/routers/user.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const multerFolder = require('../middlewares/multerFolderMw');
 const controller = require('../controllers/userController');
-//const CreateVerificator = require('../middlewares/products/createVerificator')
+const CreateVerificator = require('../middlewares/users/createVerificator');
 
 router.get('/login', controller.indexLogin);
 router.get('/register', controller.indexRegister);
 
-router.post('/save',[multerFolder('avatar', 'avatars/').single('avatar')], controller.save);
+router.post('/save', [multerFolder('avatar', 'avatars/').single('avatar'), CreateVerificator], controller.save);
 //router.put('/update/:id', [multerFolder('user', 'users/').single()], controller.update);
 //[multerFolder('user', 'usuarios/').single("avatar")],
 //router.delete('/delete/:id', controller.delete);
