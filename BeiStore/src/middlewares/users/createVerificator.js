bcedbd9dd1e22032214ab4971f14b67d6e9573b1
Inlopezl <ivanxlopez@gmail.com>
2021-08-23 19:05:14 -0300

    Arreglado error en vista

--- BeiStore/src/middlewares/users/createVerificator.js
+++ BeiStore/src/middlewares/users/createVerificator.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 
 
 module.exports = [
-    body("firstName").notEmpty().withMessage("Debe escribir un nombre"),
+    body("firstName").notEmpty().withMessage("Debe escribir un nombre").bail(),
     body("surName").notEmpty().withMessage("Debe escribir un apellido").bail(),
     body("email").notEmpty().withMessage("Debe escribir un email").bail(),
     body("password").notEmpty().withMessage("Debe escribir una contraseña").bail()
