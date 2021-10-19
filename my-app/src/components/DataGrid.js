915c0d349af94c636ba35938130b94c4a5517106
Inlopezl <ivanxlopez@gmail.com>
2021-10-19 12:08:02 -0300

    Agregada Tabla de visualizacion de productos

---
+++ my-app/src/components/DataGrid.js
@@ -0,0 +1,58 @@
+import React  from "react";
+import { Fragment, useState, useEffect } from "react";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from '@mui/x-data-grid';
+
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton />
+      <GridToolbarDensitySelector />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+
+export default function CustomToolbarGrid() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() =>{
+
+        fetch("http://localhost:8080/api/products")
+            .then(response =>{
+                    return response.json()                
+            })
+            .then( products =>{
+                    setProducts(products.products) 
+            })
+            .catch((error) => console.error(error))
+
+    }, [])
+
+    console.log(products)
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        columns={[
+            { field: 'id', width: 150, headerName: 'ID'},
+            { field: 'name', width: 150 },
+            { field: 'price', width: 150 },
+          ]}
+          rows={products}
+        components={{
+          Toolbar: CustomToolbar,
+        }}
+      />
+    </div>
+  );
+}
--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -4,6 +4,7 @@ import ProductsCatQ from "./ProductsCatQ" // Registro de la Cantidad de Categori
 import UsersQ from "./UsersQ" // Registro de la Cantidad de Usuarios
 import LastProduct from "./LastProduct" // Registro del ultimo Producto
 import LastUser from "./LastUser" // Registro del ultimo Usuario
+import DataGrid from "./DataGrid" // Tabla de datos
 
 function Main() {
     return (
@@ -20,6 +21,8 @@ function Main() {
             
             <LastUser/>
             
+            <DataGrid/>
+
         </main>
         
       );
