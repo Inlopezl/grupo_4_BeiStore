7c9707b7b2c8ddf0e7b82503bfb59e9e3d51df49
Inlopezl <ivanxlopez@gmail.com>
2021-10-19 19:25:17 -0300

    Modificacion de Header React

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -6,8 +6,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { deepPurple } from '@mui/material/colors';
-
+import "../css/main.css"
 
 function Header() {
     return (
@@ -16,20 +15,12 @@ function Header() {
             
             <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color= "inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+        <Toolbar className="toolbar">
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Beistore
           </Typography>
-          <Button color="inherit" href="http://localhost:8080/">Volver</Button>
+          <Button color="inherit" href="http://localhost:8080/"><Typography variant="h7">Volver</Typography></Button>
         </Toolbar>
       </AppBar>
     </Box>
